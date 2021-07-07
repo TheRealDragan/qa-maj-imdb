8ff7c784909d4f31f7c6a4b664e655b56050f039
Petar <petar.novakovic@code.edu.rs>
2021-07-07 20:00:44 +0200

    Vracen kostur projekta

---
+++ IMDbQA/IMDbQA/src/pages/BasePage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class BasePage {
+	public void printMessage(String message) {
+		System.out.println("Now writing the passed message: " + message);
+	}
+
+}
---
+++ IMDbQA/IMDbQA/src/pages/SignInPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class SignInPage extends BasePage {
+	private String yourNameBox = "Your name box";
+	private String emailBox = "Email Box";
+	private String passwordBox = "Password Box";
+	private String reenterPasswordBox = "Reenter Password Box";
+	private String createYourImdbAccountButton = "Create Your Imdb Account Button";
+	 public void getYourNameBox() {         
+		 printMessage(yourNameBox);
+}
+	 
+}
